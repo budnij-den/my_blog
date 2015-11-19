3ed46afd010daff30e22429b7fa9c54bf6a803ee
Metloff <genrih.metlov@gmail.com>
2015-11-19 20:13:12 +0300

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:new, :create]
+ 
+
+  def index
+    @articles = Article.order 'created_at DESC'
+    @comments = Comment.order 'created_at DESC'
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comments = Comment.order 'created_at DESC'
+  end
+  
+  def new
+        @comments = Comment.order 'created_at DESC'
+  end
+
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private 
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
