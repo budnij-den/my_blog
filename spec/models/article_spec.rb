e5607099bdd2823c19423d0b7f394bf8f2ac58f0
Metloff <genrih.metlov@gmail.com>
2015-11-27 10:56:53 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,9 @@ describe Article do
 	describe "validation" do
 		it { should validate_presence_of (:title) } 
 		it { should validate_presence_of (:text) } 
+		it { should validate_length_of(:title).is_at_most(140)}
+		it { should validate_length_of(:text).is_at_most(4000)}
+
 	end
 
 	describe "associations" do
