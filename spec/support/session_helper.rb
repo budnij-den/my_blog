d0aa6cf5a543de2fad0b6201e8e6122dc10fc0ad
Metloff <genrih.metlov@gmail.com>
2015-11-30 14:41:46 +0300

    Activate files at /spec/support

--- spec/support/session_helper.rb
+++
@@ -1,9 +0,0 @@
-def sign_up
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'user'
-    fill_in :user_password, :with => 'secure123!@#'
-    fill_in :user_password_confirmation, :with => 'secure123!@#'
-
-    click_button 'Sign up'
-
-end
---
+++ spec/support/session_helpers.rb
@@ -0,0 +1,13 @@
+    def sign_up
+        visit new_user_registration_path
+        
+        fill_in :user_email, :with => 'user6@example.com'
+        fill_in :user_username, :with => 'user6'
+        fill_in :user_password, :with => 'secure123!@#'
+        fill_in :user_password_confirmation, :with => 'secure123!@#'
+
+        click_button 'Sign up'
+
+    end
+
+
